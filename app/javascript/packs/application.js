5b8a35603008880b78a25b08019eed00eec5a475
Tito Pizarro <titomauriciopizarroochoa@gmail.com>
2021-08-09 18:57:00 -0500

    Script for suggest category

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,24 @@ var jQuery = require("jquery");
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
 require("bootstrap");
+
+$(document).ready(function() {
+
+	$(".product-name").keyup(function() {
+		let search = $(".product-name").val()
+		if (search.length < 2 ) return
+	  	for (const li of $('#category-suggested-list>li')) {
+	  		let text = li.getElementsByClassName('keyword')[0].innerText
+	  		let category = li.getElementsByClassName('category')[0].innerText
+	  	  	let result = document.getElementById('category-result')
+
+	  	  	result.innerText = 'No suggested category'
+	  		if (search.toUpperCase().includes(text.toUpperCase())) {
+		  	  	result.innerText = 'Category suggested: ' + category
+	  			break
+	  		}
+  		}
+
+	});
+
+});
